4855793ca3e6a697d27291f712067ce7f311fbfd
Alex Chen <achen715@usc.edu>
2024-04-19 06:09:39 +0000

    “done”

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,24 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool checkDays(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t> shiftsDays,
+    size_t day,
+    size_t scheduledDays
+);
 
+bool checkAvailability(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t> shiftsDays,
+    size_t day,
+    size_t scheduledDays,
+    int workerIndex
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +54,64 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    size_t k = avail[0].size(); // num of workers
+    size_t n = avail.size(); // num of days
+    vector<size_t> shifts_days(k, 0); // vector to show how many days each worker has worked.
+    sched.resize(n);
+    for (size_t i = 0; i < n; i++)
+    {
+        sched[i].assign(dailyNeed, INVALID_ID);
+    }
+    return checkDays(avail, dailyNeed, maxShifts, sched, shifts_days, 0, 0);
 }
 
+bool checkDays(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t> shiftsDays,
+    size_t day,
+    size_t scheduledDays
+)
+{
+    if (day == avail.size())
+    {
+        return true;
+    }
+    if (scheduledDays == dailyNeed) // all days filled go to next
+    {
+        return checkDays(avail, dailyNeed, maxShifts, sched, shiftsDays, day + 1, 0);
+    }
+    return checkAvailability(avail, dailyNeed, maxShifts, sched, shiftsDays, day, scheduledDays, 0);
+}
+
+// check availability for all workers in this location
+bool checkAvailability(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t> shiftsDays,
+    size_t day,
+    size_t scheduledDays,
+    int workerIndex
+)
+{
+    if (workerIndex == avail[day].size()) // base case: if all the workers are not available return false
+    {
+        return false;
+    }
+    // if available, try next day
+    if (avail[day][workerIndex] != 0 && shiftsDays[workerIndex] < maxShifts)
+    {
+        sched[day][scheduledDays] = workerIndex;
+        shiftsDays[workerIndex] += 1;
+        if (checkDays(avail, dailyNeed, maxShifts, sched, shiftsDays, day, scheduledDays + 1))
+        {
+            return true;
+        }
+        // if not available, delete the id and minus the shiftdays for backtracking
+        sched[day][scheduledDays] = INVALID_ID;
+        shiftsDays[workerIndex] -= 1; // backtrack
+    }
+    return checkAvailability(avail, dailyNeed, maxShifts, sched, shiftsDays, day, scheduledDays, workerIndex + 1);
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,20 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+int countEmptySpace(const std::string& word, int n = 0);
+void findWord(const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    int emptySpace,
+    string tempWord,
+    set<string>& wordSet);
+void findCombination(const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    int emptySpace,
+    string tempWord,
+    set<string>& wordSet,
+    int n = 0);
 
 
 // Definition of primary wordle function
@@ -22,7 +36,104 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    int empty_space = countEmptySpace(in);
+    string temp_word = "";
+    set<string> SuitableWordSet;
+    findWord(in, floating, dict, empty_space, temp_word, SuitableWordSet);
+    cout << "before" << endl;
+    for (string words : SuitableWordSet)
+    {
+        cout << words << endl;
+    }
+    return SuitableWordSet;
 }
 
 // Define any helper functions here
+// function counts how many empty spaces the word has
+int countEmptySpace(const std::string& word, int n)
+{
+    if (n >= word.size())
+    {
+        return 0;
+    }
+    if (word[n] == '-')
+    {
+        return 1 + countEmptySpace(word, n+1);
+    }
+    else
+    {
+        return countEmptySpace(word, n+1);
+    }
+}
+
+// find the words that fit the category
+void findWord(const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    int emptySpace,
+    string tempWord,
+    set<string>& wordSet)
+    {
+        // base case: if the word is in dictionary just return
+        if (tempWord.length() == in.length())
+        {
+          if (dict.find(tempWord) != dict.end())
+          {
+              wordSet.insert(tempWord);
+              return;
+          }
+        }
+
+        // if the input word has a fixed letter here, then we add to the tempWord directly
+        if (in[tempWord.size()] != '-')
+        {
+            // now recurse the next position
+            findWord(in, floating, dict, emptySpace, tempWord + in[tempWord.size()], wordSet);
+        }
+        // if the input word has '-', then use another resursive function
+        else
+        {
+            findCombination(in, floating, dict, emptySpace, tempWord, wordSet);
+        }
+    }
+
+void findCombination(const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    int emptySpace,
+    string tempWord,
+    set<string>& wordSet,
+    int n) // n here is the character position
+    {
+        // 1. if the number of empty space is equal to the floating, we only need to check the floating paritions
+        if (emptySpace <= floating.size())
+        {
+            if (n == floating.size())
+            {
+                return;
+            }
+            string temp_floating = floating;
+            temp_floating.erase(temp_floating.find(floating[n]), 1);
+            findWord(in, temp_floating, dict, emptySpace - 1, tempWord + floating[n], wordSet);
+            findCombination(in, floating, dict, emptySpace, tempWord, wordSet, n + 1);
+        }
+        // 2. if there are more empty spaces, then we need to check from a to z
+        else
+        {
+            for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                // if the letter is not in floating
+                if (floating.find(letter) == string::npos)
+                {
+                    findWord(in, floating, dict, emptySpace - 1, tempWord + letter, wordSet);
+                }
+                // if the letter is in floating
+                else
+                {
+                    string temp_floating = floating;
+                    temp_floating.erase(temp_floating.find(letter), 1);
+                    findWord(in, temp_floating, dict, emptySpace - 1, tempWord + letter, wordSet);
+                }
+            }
+        }
+    }
